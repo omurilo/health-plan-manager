9200e2b30c7dbb143ff839cdc1506bc3633f9695
Murilo Alves <murilo.alves@routeasy.com.br>
2022-03-26 22:51:17 -0300

    feat: create services instance
    - clients
    - beneficiaries
    - providers

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,11 @@
+import ClientsService from "./clients/index.js";
+import PlanProvidersService from "./providers/index.js";
+import BeneficiariesService from "./beneficiaries/index.js";
+
 export default class Services {
   constructor() {
+    this.clients = new ClientsService()
+    this.planProviders = new PlanProvidersService()
+    this.beneficiaries = new BeneficiariesService({  client: this.clients, planProviders: this.planProviders });
   }
 }
