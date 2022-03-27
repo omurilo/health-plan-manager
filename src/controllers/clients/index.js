1819d0cf2565e024a1039c780b12f763e8a63c3d
Murilo Alves <murilo.alves@routeasy.com.br>
2022-03-26 23:27:37 -0300

    feat: create clients, providers and beneficiaries controllers

---
+++ src/controllers/clients/index.js
@@ -0,0 +1,17 @@
+export default class ClientsController {
+  constructor({ service }) {
+    this.service = service
+  }
+
+  create(data) {
+    return this.service.create(data)
+  }
+
+  get(id) {
+    return this.service.get(id)
+  }
+
+  find(query) {
+    return this.service.find(query)
+  }
+}
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,14 @@
+import ClientsController from "./clients/index.js";
+import PlanProvidersController from "./providers/index.js";
+import BeneficiariesController from "./beneficiaries/index.js";
+
 import Services from "../services/index.js"
 
 export default class Controllers {
   constructor() {
     const services = new Services();
+    this.clients = new ClientsController({ service: services.clients })
+    this.planProviders = new PlanProvidersController({ service: services.planProviders })
+    this.beneficiaries = new BeneficiariesController({ service: services.beneficiaries })
   }
 }
