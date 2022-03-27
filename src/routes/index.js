2d473b9fb5b2788fe262805b7e88026f1b5bf8f4
Murilo Alves <murilo.alves@routeasy.com.br>
2022-03-26 23:31:19 -0300

    feat: create routes to retrieve and  add new entity
    - clients
    - providers
    - beneficiaries

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { once } from 'events'
+import QueryString from 'querystring'
 
 export default class Routes {
   constructor({ controllers }) {
@@ -6,15 +7,94 @@ export default class Routes {
   }
 
   async routes(request, response) {
-    const { url, method } = request;
+    const { url: unparsedUrl, method } = request;
+    let query = null
+    let params = null
+    let result = null
 
-    if (method === 'GET' && url === '/') {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.write(JSON.stringify({
-        statusCode: 200,
-        body: 'Hello World!',
-      }));
-      response.end();
+    if (unparsedUrl.includes('?')) {
+      query = QueryString.parse(unparsedUrl.split('?')[1])
+    }
+
+    if (unparsedUrl.includes('/')) {
+      const [, url, ...rest] = unparsedUrl.split('/')
+      params = rest
+    }
+
+    if (method === 'GET') {
+      if (unparsedUrl === '/') {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.write(JSON.stringify({
+          statusCode: 200,
+          body: 'Hello World!',
+        }));
+        response.end();
+      }
+
+      if (unparsedUrl.includes('/beneficiaries')) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        if (!params) {
+          result = await this.controllers.beneficiaries.find(query);
+        } else {
+          result = await this.controllers.beneficiaries.get(params[0]);
+        }
+
+        return response.end(JSON.stringify(result));
+      }
+
+      if (unparsedUrl.includes('/clients')) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        if (!params) {
+          result = await this.controllers.clients.find(query);
+        } else {
+          result = await this.controllers.clients.get(params[0]);
+        }
+
+        return response.end(JSON.stringify(result));
+      }
+
+      if (unparsedUrl.includes('/providers')) {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+
+        if (!params) {
+          result = await this.controllers.planProviders.find(query);
+        } else {
+          result = await this.controllers.planProviders.get(params[0]);
+        }
+
+        return response.end(JSON.stringify(result));
+      }
+    }
+
+    if (method === 'POST') {
+      if (unparsedUrl === '/beneficiaries') {
+        const body = await once(request, 'data')
+        const data = JSON.parse(body);
+
+        const result = this.controllers.beneficiaries.create(data);
+
+        return response.end(JSON.stringify(result))
+      }
+
+      if (unparsedUrl === '/clients') {
+        const body = await once(request, 'data')
+        const data = JSON.parse(body);
+
+        const result = this.controllers.clients.create(data);
+
+        return response.end(JSON.stringify(result))
+      }
+
+      if (unparsedUrl === '/providers') {
+        const body = await once(request, 'data')
+        const data = JSON.parse(body);
+
+        const result = this.controllers.planProviders.create(data);
+
+        return response.end(JSON.stringify(result))
+      }
     }
   }
 
